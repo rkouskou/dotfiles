a78be8e20b849eee972a934bd7d0c83a016fce51
rigask <rigas.kouskouridas@helsing.ai>
2023-07-30 18:15:58 +0000

    Added help

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 basics(){
+  # installing basic libs
+  echo "Installing zsh, tmux, ranger, helix and pyright"
   sudo apt install zsh --yes
   sudo apt install tmux --yes
   sudo apt install ranger --yes
@@ -8,10 +10,10 @@ basics(){
   sudo apt update
   sudo apt install helix --yes
   sudo npm install --location=global pyright
-  echo "Testing to see if 2"
 }
 
 scratch(){
+  echo "Installing personalised libs for zsh, tmux and helix"
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
   git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
   cp .tmux.conf $HOME
@@ -20,14 +22,19 @@ scratch(){
   cp config.toml $HOME/.config/helix/
 }
 
-while getopts "rs" OPTION; do
+while getopts "rs:h" OPTION; do
   case "$OPTION" in
     s)
+        echo "Installing personalised libs on a new Linux instance"
         basics
         scratch
         ;;
     r) 
+        echo "Reparing installed libs"
         basics
+        ;;
+    h) 
+        echo "Script options: [-r] repair installation, [-s] install from scratch"
         ;; 
   esac
 done
