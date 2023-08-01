78812c05c8ae105920d8a68e497cc6800ca75c86
Rigas Kouskouridas <rkouskou@users.noreply.github.com>
2023-08-01 20:00:42 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
+echo "Installing zsh, tmux, ranger, helix and pyright"
+sudo apt install zsh --yes
+sudo apt install tmux --yes
+sudo apt install ranger --yes
+sudo apt install ncdu --yes
+sudo add-apt-repository ppa:maveonair/helix-editor
+sudo apt update
+sudo apt install helix --yes
+sudo npm install --location=global pyright
 
-basics(){
-  # installing basic libs
-  echo "Installing zsh, tmux, ranger, helix and pyright"
-  sudo apt install zsh --yes
-  sudo apt install tmux --yes
-  sudo apt install ranger --yes
-  sudo apt install ncdu --yes
-  sudo add-apt-repository ppa:maveonair/helix-editor
-  sudo apt update
-  sudo apt install helix --yes
-  sudo npm install --location=global pyright
-}
 
 scratch(){
   echo "Installing personalised libs for zsh, tmux and helix"
@@ -23,16 +20,11 @@ scratch(){
   cp config.toml $HOME/.config/helix/
 }
 
-while getopts "rs:h" OPTION; do
+while getopts "r:h" OPTION; do
   case "$OPTION" in
-    s)
-        echo "Installing personalised libs on a new Linux instance"
-        basics
-        scratch
-        ;;
     r) 
         echo "Reparing installed libs"
-        basics
+        scratch
         ;;
     h) 
         echo "Script options: [-r] repair installation, [-s] install from scratch"
